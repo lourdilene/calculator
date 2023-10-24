9f3df65f5dbe82d8778d3cc1e4114672cd60c0a5
Lourdilene Souza <lourdilene.souza@gmail.com>
2023-10-24 11:28:17 -0300

    function refactoring

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,14 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+/**Everything that comes from the HTML input is string */
+function add() {
+  const calcDescription = `${currentResult} + ${userInput.value}`;
+  currentResult = currentResult + parseInt(userInput.value);
+  outputResult(currentResult, calcDescription);
+}
 
-let calculationDescription = "(" + defaultResult + " + 10) * 3 / 2 - 1";
-
-outputResult(currentResult, calculationDescription);
+/**addBtn.addEventListener("click", add); The "add" function is called
+ * without the "()" as it indicates that the function will only be called
+ * if there is a mouse click */
+addBtn.addEventListener("click", add);
